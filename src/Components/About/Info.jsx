3b38bd0ca0cc1486b28a86a0a30c624b6bee7e43
DevJaysz <149165379+DevJaysz@users.noreply.github.com>
2024-05-13 10:54:34 +0800

    .

--- src/Components/About/Info.jsx
+++ src/Components/About/Info.jsx
@@ -7,13 +7,13 @@ function Info() {
         <i className="bx bx-code-alt about-icon"></i>
 
         <h3 className="about-title">Experience</h3>
-        <span className="about-subtitle">1 YEAR</span>
+        <span className="about-subtitle">1.5 YEARS</span>
       </div>
       <div className="about-box" data-aos="zoom-in" data-aos-duration="1000">
         <i className="bx bx-medal about-icon"></i>
 
         <h3 className="about-title">Completed</h3>
-        <span className="about-subtitle">PROJECTS</span>
+        <span className="about-subtitle">2 PROJECTS</span>
       </div>
       <div className="about-box" data-aos="zoom-in" data-aos-duration="1500">
         <i className="bx bx-support about-icon"></i>
